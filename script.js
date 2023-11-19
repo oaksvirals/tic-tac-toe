1e4b987f8f8525d570a6c36263bea8bfcbd182c7
oaksvirals <oaksvirals@gmail.com>
2023-11-19 11:29:13 -0600

    Styling to assist with coding
    Added basic styling elementsd and alignments to assist with
    coding the DOM manipulating elements.

--- script.js
+++ script.js
@@ -1,87 +1,3 @@
-// ---- DOM Controller ----
-// 1) Build play area with buttons
-const gameArea = document.querySelector('.game');
-const btn0 = document.createElement('button');
-const btn1 = document.createElement('button');
-const btn2 = document.createElement('button');
-const btn3 = document.createElement('button');
-const btn4 = document.createElement('button');
-const btn5 = document.createElement('button');
-const btn6 = document.createElement('button');
-const btn7 = document.createElement('button');
-const btn8 = document.createElement('button');
-
-gameArea.appendChild(btn0);
-gameArea.appendChild(btn1);
-gameArea.appendChild(btn2);
-gameArea.appendChild(btn3);
-gameArea.appendChild(btn4);
-gameArea.appendChild(btn5);
-gameArea.appendChild(btn6);
-gameArea.appendChild(btn7);
-gameArea.appendChild(btn8);
-
-btn0.setAttribute('class', 'btn0');
-btn1.setAttribute('class', 'btn1');
-btn2.setAttribute('class', 'btn2');
-btn3.setAttribute('class', 'btn3');
-btn4.setAttribute('class', 'btn4');
-btn5.setAttribute('class', 'btn5');
-btn6.setAttribute('class', 'btn6');
-btn7.setAttribute('class', 'btn7');
-btn8.setAttribute('class', 'btn8');
-
-btn0.addEventListener('click', selectSquare);
-btn1.addEventListener('click', selectSquare);
-btn2.addEventListener('click', selectSquare);
-btn3.addEventListener('click', selectSquare);
-btn4.addEventListener('click', selectSquare);
-btn5.addEventListener('click', selectSquare);
-btn6.addEventListener('click', selectSquare);
-btn7.addEventListener('click', selectSquare);
-btn8.addEventListener('click', selectSquare);
-
-function selectSquare() {
-    switch (this) {
-        case btn0:
-            this.textContent = currentPlayer;
-            game().playRound(0, 0);
-        break;
-        case btn1:
-            this.textContent = currentPlayer;
-            game().playRound(0, 1);
-        break;
-        case btn2:
-            this.textContent = currentPlayer;
-            game().playRound(0, 2);
-        break;
-        case btn3:
-            this.textContent = currentPlayer;
-            game().playRound(1, 0);
-        break;
-        case btn4:
-            this.textContent = currentPlayer;
-            game().playRound(1, 1);
-        break;
-        case btn5:
-            this.textContent = currentPlayer;
-            game().playRound(1, 2);
-        break;
-        case btn6:
-            this.textContent = currentPlayer;
-            game().playRound(2, 0);
-        break;
-        case btn7:
-            this.textContent = currentPlayer;
-            game().playRound(2, 1);
-        break;
-        case btn8:
-            this.textContent = currentPlayer;
-            game().playRound(2, 2);
-        break;
-    }
-};
-
 // ---- Gameboard Setup ----
 // 1) Initialize newBoard at start.
 const gameboard = (function() {
@@ -233,7 +149,13 @@ function game() {
 
         };
 
+        const restartButton = document.querySelector('.btn-restart');
+        restartButton.addEventListener('click', restartGame);
+
         function restartGame() {
+
+            winner = null;
+
             board[0][0] = 0;
             board[0][1] = 0;
             board[0][2] = 0;
@@ -244,9 +166,100 @@ function game() {
             board[2][1] = 0;
             board[2][2] = 0;
 
-            winner = null;
+            btn0.textContent = '';
+            btn1.textContent = '';
+            btn2.textContent = '';
+            btn3.textContent = '';
+            btn4.textContent = '';
+            btn5.textContent = '';
+            btn6.textContent = '';
+            btn7.textContent = '';
+            btn8.textContent = '';
         };
 
-        return { playRound, restartGame }
+        return { playRound }
 
+};
+
+// ---- DOM Controller ----
+const gameArea = document.querySelector('.game');
+const btn0 = document.createElement('button');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const btn3 = document.createElement('button');
+const btn4 = document.createElement('button');
+const btn5 = document.createElement('button');
+const btn6 = document.createElement('button');
+const btn7 = document.createElement('button');
+const btn8 = document.createElement('button');
+
+gameArea.appendChild(btn0);
+gameArea.appendChild(btn1);
+gameArea.appendChild(btn2);
+gameArea.appendChild(btn3);
+gameArea.appendChild(btn4);
+gameArea.appendChild(btn5);
+gameArea.appendChild(btn6);
+gameArea.appendChild(btn7);
+gameArea.appendChild(btn8);
+
+btn0.setAttribute('class', 'btn0');
+btn1.setAttribute('class', 'btn1');
+btn2.setAttribute('class', 'btn2');
+btn3.setAttribute('class', 'btn3');
+btn4.setAttribute('class', 'btn4');
+btn5.setAttribute('class', 'btn5');
+btn6.setAttribute('class', 'btn6');
+btn7.setAttribute('class', 'btn7');
+btn8.setAttribute('class', 'btn8');
+
+btn0.addEventListener('click', selectSquare);
+btn1.addEventListener('click', selectSquare);
+btn2.addEventListener('click', selectSquare);
+btn3.addEventListener('click', selectSquare);
+btn4.addEventListener('click', selectSquare);
+btn5.addEventListener('click', selectSquare);
+btn6.addEventListener('click', selectSquare);
+btn7.addEventListener('click', selectSquare);
+btn8.addEventListener('click', selectSquare);
+
+function selectSquare() {
+    switch (this) {
+        case btn0:
+            this.textContent = currentPlayer;
+            game().playRound(0, 0);
+        break;
+        case btn1:
+            this.textContent = currentPlayer;
+            game().playRound(0, 1);
+        break;
+        case btn2:
+            this.textContent = currentPlayer;
+            game().playRound(0, 2);
+        break;
+        case btn3:
+            this.textContent = currentPlayer;
+            game().playRound(1, 0);
+        break;
+        case btn4:
+            this.textContent = currentPlayer;
+            game().playRound(1, 1);
+        break;
+        case btn5:
+            this.textContent = currentPlayer;
+            game().playRound(1, 2);
+        break;
+        case btn6:
+            this.textContent = currentPlayer;
+            game().playRound(2, 0);
+        break;
+        case btn7:
+            this.textContent = currentPlayer;
+            game().playRound(2, 1);
+        break;
+        case btn8:
+            this.textContent = currentPlayer;
+            game().playRound(2, 2);
+        break;
+    }
 };
